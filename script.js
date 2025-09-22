ad21f74e726c192a83c9a084b3fbe0c23b977b95
Ahmed Marwan <132778334+AhmedMarwan5@users.noreply.github.com>
2025-09-22 22:07:04 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// const form = document.querySelector(".form");
+
+// form.addEventListener("submit", function (e){
+//     const data = new FormData(form);
+//     console.log(data)
+//     e.preventDefault();
+//     console.log(e);
+// });
